b653746253aa75c8e84876bcd349834837e43b1f
Jared Burnham <zlolris97@gmail.com>
2019-07-05 15:56:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,8 +14,8 @@ def monopoly_with_third_tier
   monopoly = {
     railroads:{
       pieces: 4,
-      names:{}
-      ,rent_in_dollars:{
+      names:{},
+      rent_in_dollars:{
         one_piece_owned: 25
       }
       
